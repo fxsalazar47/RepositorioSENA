812e622fc03fd468b45b77d99c8adbc13d891c81
Fabian Salazar <vxlegend47@gmail.com>
2025-08-01 14:58:57 -0500

    Taller Completo IF

--- if_elif_else28jul.py
+++ if_elif_else28jul.py
@@ -125,10 +125,127 @@
 
 # 13 ---- Suma los dos primeros elementos de la lista [4, 6, 2, 8]. Si la suma es mayor que 10, muestra “Suma alta”, de lo contrario, muestra “Suma baja”. ----
 
-list = [float(input(f"Ingrese un primer numero: ")) , float(input(f"Ingrese un segundo numero: ")) , float(input(f"Ingrese un tercer numero: ")) , float(input(f"Ingrese un cuarto numero: "))]
+# list = [float(input(f"Ingrese un primer numero: ")) , float(input(f"Ingrese un segundo numero: ")) , float(input(f"Ingrese un tercer numero: ")) , float(input(f"Ingrese un cuarto numero: "))]
 
-if list[0] + list[1] > 10:
-    print(f"Suma alta")
-else:
-    print("Suma baja")
+# if list[0] + list[1] > 10:
+#     print(f"Suma alta")
+# else:
+#     print("Suma baja")
+
+# 14 ---- Dada la lista ["Ana", "Luis", "Pedro", "Marta"], muestra el último nombre. Si ese nombre es “Marta”, muestra “Nombre correcto”, si no, “Nombre diferente”. ----
+
+# nom = [input(f"Ingresa un primer nombre: ") , input(f"Ingresa un segundo nombre: ") , input(f"Ingresa un tercer nombre: ") , input(f"Ingresa un cuarto nombre: ")]
+
+# if nom[3] == "Marta" or nom[3] == "marta":
+#     print(f"Nombre correcto! {nom[3]}")
+# else:
+#     print(f"Nombre incorrecto! {nom[3]}")
+
+# 15 ---- Crea una lista con tres colores. Cambia el segundo color solo si es igual a "azul", y muestra la lista actualizada. ----
+
+# list = [input(f"Ingresa un color: ") , input(f"Ingresa otro color: ") , input(f"Ingresa un último color: ")]
+
+# if list[1] == "Azul" or list[1] == "azul":
+#     list[1]="Púrpura"
+#     print(f"La lista actualizada es: {list}")
+# else:
+#     print(f"La lista permanece igual")
+
+# 16 ---- Crea una tupla con los valores (5, 8, 12, 20). Si el primer valor es menor que el último, muestra “Orden ascendente”, si no, “Orden descendente”. ----
+
+# tup = (float(input(f"Ingrese un numero (1/4): ")) , float(input(f"Ingrese un numero (2/4): ")) , float(input(f"Ingrese un numero (3/4): ")) , float(input(f"Ingrese un numero (4/4): ")))
+
+# if tup[0] < tup[3]:
+#     print(f"Orden ascendente")
+# else:
+#     print(f"Orden descendente")
+
+# 17 ---- Dada la tupla (25, 32, 28), verifica si el segundo valor es mayor a 30. Si lo es, muestra “Edad mayor a 30”, si no, “Edad menor o igual a 30”. ----
+
+# tup = (int(input(f"Ingrese un numero (1/3): ")) , int(input(f"Ingrese un numero (2/3): ")) , int(input(f"Ingrese un numero (3/3): ")))
+
+# if tup[1] > 30:
+#     print(f"Edad ({tup[1]}) mayor a 30")
+# else:
+#     print(f"Edad ({tup[1]}) menor o igual a 30")
+
+# 18 ---- Convierte la tupla (1, 2, 3) a lista, y cambia el segundo valor a 10 solo si es igual a 2, luego a convertirla a convertirla a tupla y muestrala ----
+
+# tup = (int(input(f"Ingrese un numero (1/3): ")) , int(input(f"Ingrese un numero (2/3): ")) , int(input(f"Ingrese un numero (3/3): ")))
+
+# list = list(tup)
+
+# if list[1] == 2:
+#     list[1] = 10
+#     tup = tuple(list)
+#     print(f"Numeros ingresados: {tup}")
+# else:
+#     tup = tuple(list)
+#     print(f"Numeros ingresados: {tup}")
+
+# 19 ---- Dada la tupla (4, 9), accede al segundo valor. Si es mayor que 5, muestra "Coordenada alta". sino, "Coordenada baja"
+
+# tup = (int(input(f"Ingrese una coordenada: ")) , int(input(f"Ingrese otra coordenada: ")))
+
+# if tup[1] > 5:
+#     print(f"Coordenada alta: {tup[1]}")
+# else:
+#     print(f"Coordenada baja: {tup[1]}")
+
+# 20 ---- Compara las tuplas (3, 4) y (3, 5) son iguales, Si lo son, muestra "Tuplas iguales", sino, "Tuplas diferentes"
+
+# tup1 = (int(input(f"Ingrese un numero (1/2): ")) , int(input(f"Ingrese un numero (2/2): ")))
+# tup2 = (int(input(f"-------------------------------------------\nIngrese otros numero (1/2): ")) , int(input(f"Ingrese otros numero (2/2): ")))
+
+# if tup1 == tup2:
+#     print(f"Tuplas iguales")
+# else:
+#     print(f"Tuplas diferentes")
+
+# 21 ---- Crea un diccionario con {"nombre" : "Juan" , "edad" : 17}. Si la edad es mayor o igual a 18, muestra "Adulto", si no, muestra "Menor de edad"
+
+# dat = {"nombre" : input(f"Ingresa tu nombre: ") , "edad" : int(input(f"Ingresa tu edad: "))}
+
+# if dat["edad"] >= 18:
+#     print(f"Hola {dat['nombre']}! Tienes {dat['edad']} años y eres adulto!")
+# else:
+#     print(f"Hola {dat['nombre']}! Tienes {dat['edad']} años y eres menor de edad!")
+
+# 22
+
+# dat = {"nombre" : input(f"Ingresa tu nombre: ") , "edad" : int(input(f"Ingresa tu edad: "))}
+
+# if dat["edad"] > 18:
+#     dat["edad"] = 21
+#     print(f"Datos ingresados: {dat}")
+# else:
+#     print(f"Datos ingresados: {dat}")
+
+# 23
+
+# dat = {"nombre" : input(f"Ingresa su nombre: ")}
+
+# if dat.get("ciudad") == None :
+#     dat["ciudad"] = "Bogotá"
+#     print(f"{dat}")
+# else:
+#     print(f"{dat}")
+
+# 24
+
+# dat = {"producto" : input(f"Ingresa el nombre del producto: ") , "precio" : float(input(f"Ingrese el precio: $"))}
+
+# if dat.get("precio") == dat["precio"]:
+#     print(f"El precio es: ${dat['precio']}")
+# else:
+#     print(f"No hay precio")    
+
+# 25
+
+# dat = {"producto" : input(f"Ingresa el nombre del producto: ") , "precio" : float(input(f"Ingrese el precio: $"))}
+
+# if dat.get("producto") == dat["producto"]:
+#     print(f"El precio de {dat['producto']} es: ${dat['precio']}")
+# else:
+#     print(f"Producto No Disponible")    
 
